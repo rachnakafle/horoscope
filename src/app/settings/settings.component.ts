7bcb9730f7945a402e39ae525fcbbc5407076c6f
rachnakafle <rachanakafle99@gmail.com>
2022-02-23 16:04:00 +0545

    Added custom time

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SettingsComponent implements OnInit {
   constructor() {}
   selectedDateFormat!: any ;
-  currentdate = new Date();
+  currentdate = new Date(); 
 
   dateFormats = [
     {
